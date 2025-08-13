51dcae6afbce27d466be9d43f984637529a917bc
Gaurab19 <gaurab625@gmail.com>
2025-08-13 21:15:56 +0545

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+// .eslintrc.cjs
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    // If you also use prettier in the project (even if disabled in IDE)
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    // Disable stylistic rules but still allow auto-fix
+    'style/*': 'off',
+    'format/*': 'off',
+    '*-indent': 'off',
+    '*-spacing': 'off',
+    '*-spaces': 'off',
+    '*-order': 'off',
+    '*-dangle': 'off',
+    '*-newline': 'off',
+    '*quotes': 'off',
+    '*semi': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '*.js',
+        '*.jsx',
+        '*.ts',
+        '*.tsx',
+        '*.vue',
+        '*.html',
+        '*.md',
+        '*.json',
+        '*.yaml',
+        '*.yml',
+        '*.css',
+        '*.scss',
+        '*.less'
+      ],
+      parserOptions: {
+        ecmaVersion: 'latest'
+      }
+    }
+  ]
+}

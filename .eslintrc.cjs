f8bfc9c96d8b7dc965519d934ceb563547585cb4
Gaurab19 <gaurab625@gmail.com>
2025-08-16 16:20:24 +0545

    fix-lint-issue-fixed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,51 +3,51 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
     // If you also use prettier in the project (even if disabled in IDE)
-    'prettier'
+    "prettier",
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
     // Disable stylistic rules but still allow auto-fix
-    'style/*': 'off',
-    'format/*': 'off',
-    '*-indent': 'off',
-    '*-spacing': 'off',
-    '*-spaces': 'off',
-    '*-order': 'off',
-    '*-dangle': 'off',
-    '*-newline': 'off',
-    '*quotes': 'off',
-    '*semi': 'off'
+    "style/*": "off",
+    "format/*": "off",
+    "*-indent": "off",
+    "*-spacing": "off",
+    "*-spaces": "off",
+    "*-order": "off",
+    "*-dangle": "off",
+    "*-newline": "off",
+    "*quotes": "off",
+    "*semi": "off",
   },
   overrides: [
     {
       files: [
-        '*.js',
-        '*.jsx',
-        '*.ts',
-        '*.tsx',
-        '*.vue',
-        '*.html',
-        '*.md',
-        '*.json',
-        '*.yaml',
-        '*.yml',
-        '*.css',
-        '*.scss',
-        '*.less'
+        "*.js",
+        "*.jsx",
+        "*.ts",
+        "*.tsx",
+        "*.vue",
+        "*.html",
+        "*.md",
+        "*.json",
+        "*.yaml",
+        "*.yml",
+        "*.css",
+        "*.scss",
+        "*.less",
       ],
       parserOptions: {
-        ecmaVersion: 'latest'
-      }
-    }
-  ]
-}
+        ecmaVersion: "latest",
+      },
+    },
+  ],
+};

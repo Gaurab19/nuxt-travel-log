51f275dfaa422cd7e69531964692a01371f71096
Gaurab19 <gaurab625@gmail.com>
2025-09-19 16:24:43 +0545

    auth-fixed

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,24 +1,9 @@
 import { auth } from "../../lib/auth";
 
-// export default defineEventHandler(async (event) => {
-//   const session = await auth.api.getSession({
-//     headers: event.headers,
-//   });
-//   event.context.user = session?.user;
-//   if (!session.user) {
-//     return sendRedirect(event, "/");
-//   }
-// });
 export default defineEventHandler(async (event) => {
-  // Skip middleware for static assets and API routes
-  if (
-    event.path.startsWith("/_nuxt")
-    || event.path.startsWith("/api")
-    || event.path.startsWith("/favicon.ico")
-  ) {
-    return;
-  }
-  const session = await auth.api.getSession({ headers: event.headers });
+  const session = await auth.api.getSession({
+    headers: event.headers,
+  });
   event.context.user = session?.user;
   if (!session.user) {
     return sendRedirect(event, "/");
